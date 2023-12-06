f1ba8019646aa42f01b33fdcbdbbdfe9812a8597
Sooyung Kim <kimx1279@gmail.com>
2023-12-05 19:29:09 -0700

    Create vendor, working happy path

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -10,7 +10,21 @@ class Api::V0::VendorsController < ApplicationController
     render json: VendorSerializer.new(Vendor.find(params[:id]))
   end
 
+  def create
+    vendor = Vendor.new(vendor_params)
+    
+    if vendor.save 
+      render json: VendorSerializer.new(vendor), status: :created
+    else
+      render json: { errors: [{ title: "Validation failed: Contact name can't be blank, Contact phone can't be blank", status: "400" }]}
+    end
+  end
+
   private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+  end
 
   def not_found_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -35,6 +35,7 @@ describe 'Vendors API' do
   end
 
   it 'only provides info if vendor id exist' do
+    # 4. Get one vendor, sad path
     get "/api/v0/vendors/1"
     
     expect(response).to_not be_successful
@@ -43,7 +44,52 @@ describe 'Vendors API' do
     data = JSON.parse(response.body, symbolize_names: true)
     expect(data[:errors]).to be_a(Array)
     expect(data[:errors].first[:status]).to eq("404")
-    require 'pry'; binding.pry
     expect(data[:errors].first[:title]).to eq("Couldn't find Vendor with 'id'=1")
+  end
+
+  it 'can create a vendor' do
+    # 5. Create a vendor, happy path
+    vendor_params = ({
+      "name": "Buzzy Bees",
+      "description": "local honey and wax products",
+      "contact_name": "Berly Couwer",
+      "contact_phone": "8389928383",
+      "credit_accepted": false
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+    
+    post "/api/v0/vendors", headers: headers, params: JSON.generate(vendor: vendor_params)
+
+    created_vendor = Vendor.last
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(created_vendor.name).to eq(vendor_params[:name])
+    expect(created_vendor.description).to eq(vendor_params[:description])
+    expect(created_vendor.contact_name).to eq(vendor_params[:contact_name])
+    expect(created_vendor.contact_phone).to eq(vendor_params[:contact_phone])
+    expect(created_vendor.credit_accepted).to eq(vendor_params[:credit_accepted])
+  end
+
+  it 'does not create vendor if not all attributes are provided' do
+    # 5. Create a vendor, sad path
+    vendor_params = ({
+      "name": "Buzzy Bees",
+      "description": "local honey and wax products",
+      "credit_accepted": false
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+    
+    post "/api/v0/vendors", headers: headers, params: JSON.generate(vendor: vendor_params)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("400")
+    expect(data[:errors].first[:title]).to eq("Validation failed: Contact name can't be blank, Contact phone can't be blank")
   end
 end

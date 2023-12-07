85bdf9abd951a8cc089ece8cd3865ad36596deec
Sooyung Kim <kimx1279@gmail.com>
2023-12-06 23:41:17 -0700

    Add passing sad path

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -6,8 +6,8 @@ class Api::V0::MarketVendorsController < ApplicationController
     market_vendor = MarketVendor.new(market_vendor_params)
     if market_vendor.save
       render json: MarketVendorSerializer.new(market_vendor), status: :created
-    # else
-    #   render_unprocessable_entity_response(market_vendor)
+    else
+      render_unprocessable_entity_response(market_vendor)
     end
   end
 
@@ -22,6 +22,6 @@ class Api::V0::MarketVendorsController < ApplicationController
   end
 
   def render_unprocessable_entity_response(exception)
-    render json: ErrorSerializer.new(ValidationErrorMessage.new(exception.errors.full_messages.join(', '), 400)).serialize_json, status: :bad_request
+    render json: ErrorSerializer.new(ValidationErrorMessage.new(exception.errors.full_messages.join(', '), 404)).serialize_json, status: :not_found
   end
 end
--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -16,4 +16,25 @@ describe 'Market Vendors API' do
     expect(response).to be_successful
     expect(response.status).to eq(201)
   end
+
+  it 'does not create if market or vendor does not exist' do
+    # 8. Create a market vendor, sad path
+    vendor = create(:vendor).id
+
+    market_vendor_params= {
+      "market_id": 1234,
+      "vendor_id": vendor
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v0/market_vendors', headers: headers, params:  JSON.generate(market_vendor: market_vendor_params) 
+    
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Validation failed: Market must exist")
+  end
 end

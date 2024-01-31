3a3f5b1bb1f7c288929c3943048821a4384fa8bd
Sooyung Kim <kimx1279@gmail.com>
2024-01-30 22:23:00 -0700

    Add another test

--- spec/poros/atm_spec.rb
+++ spec/poros/atm_spec.rb
@@ -20,5 +20,24 @@ RSpec.describe Atm do
       expect(data1.lon).to eq(2)
       expect(data1.distance).to eq(2)
     end
+
+    it 'exist' do
+      data = {
+        address: {
+          freeformAddress: 'test2'
+        },
+        position: { 
+          lat: 1,
+          lon: 2
+        },
+        score: 2
+      }
+      data1 = Atm.new(data)
+      expect(data1.name).to eq('ATM')
+      expect(data1.address).to eq('test2')
+      expect(data1.lat).to eq(1)
+      expect(data1.lon).to eq(2)
+      expect(data1.distance).to eq(2)
+    end
   end
 end
